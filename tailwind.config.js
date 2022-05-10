b8758405f21dfcc4c546bf8ac6690031fa30b084
sama21619 <82813022+sama21619@users.noreply.github.com>
2022-05-10 20:53:34 +0700

    cc

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        'screen-navbar-player': 'calc(100vh - 6rem - 6rem)'
+
+      }
+      
+    },
+  },
+  plugins: [],
+}
