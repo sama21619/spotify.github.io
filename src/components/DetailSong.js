ed2d0afee9e74b471f6da68363fbfe756dde0bf7
sama21619 <82813022+sama21619@users.noreply.github.com>
2022-05-20 09:20:42 +0700

    oki

--- src/components/DetailSong.js
+++ src/components/DetailSong.js
@@ -8,10 +8,10 @@ export default function DetailSong(){
              <h2 className='text-cyan-500 font-bold'>Now playing</h2>
             <h2 className='text-neutral-400 text-2xl'>{song.name}</h2>
             <div className='w-[240px] m-auto mt-10'>
-                <img className='w-full' src={song.links.images[0].url} alt='avatar' />
+                <img className='lg:w-full md:w-full sm:w-[200px] xs:w-[150px]' src={song.links.images[0].url} alt='avatar' />
             </div>
             <div className='flex justify-evenly items-center mt-10'>
-            <img className='w-[70px] rounded-full' src='https://scontent.fsgn8-1.fna.fbcdn.net/v/t39.30808-6/269967632_3226490621009922_1066982041896637066_n.jpg?_nc_cat=111&ccb=1-6&_nc_sid=174925&_nc_ohc=8IF9CpaXymoAX-r773v&_nc_ht=scontent.fsgn8-1.fna&oh=00_AT_rANvpHEHgIJ3uuX5ADC6QO7lrn-pNZ00SUx8BI9_miw&oe=627DE06D' alt='avatar' />
+            <img className='lg:w-[70px] md:w-[70px] sm:w-[50px] xs:w-[30px] rounded-full' src={song.links.images[1].url} alt='rounded-avatar' />
             <span className='text-xl text-white'>{song.author}</span>
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
       }
       
     },
+
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+  }
+
+    
   },
   plugins: [],
 }
