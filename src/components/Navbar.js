bcd4fdd575b1d49c75595b4b71d5572113b9e56d
sama21619 <82813022+sama21619@users.noreply.github.com>
2022-05-20 09:25:33 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ export default function Navbar(){
     return (
         <div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
             <i className="fa fa-spotify mr-5"></i>
-            Sờ pót fi fai
+            Spotify
         </div>
     )
 }
